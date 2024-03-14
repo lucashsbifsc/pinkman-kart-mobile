25c422f9da0828dea33c147cbca18e923f64dfa4
Lucas Henrique Santos de Bairros <124683425+lucashsbifsc@users.noreply.github.com>
2024-03-14 08:58:05 -0300

    Editado Karts de acordo com as outras classes

--- app/src/main/java/com/ifscgaspar/sistemapinkmankart/modelo/Karts.java
+++ app/src/main/java/com/ifscgaspar/sistemapinkmankart/modelo/Karts.java
@@ -3,21 +3,21 @@ package com.ifscgaspar.sistemapinkmankart.modelo;
 import java.sql.Date;
 
 public class Karts {
-    private Integer id;
+    private long id;
     private String cor;
     private String modelo;
     private String marca;
-    private int ano;
-    private int quantidade;
-    private Date data_entrada;
-    private float preco;
-    private int fornecedorCnpj;
+    private Long ano;
+    private Long quantidade;
+    private Date dataEntrada;
+    private Long preco;
+    private Long fornecedorCnpj;
 
-    public Integer getId() {
+    public long getId() {
         return id;
     }
 
-    public void setId(Integer id) {
+    public void setId(long id) {
         this.id = id;
     }
 
@@ -27,5 +27,61 @@ public class Karts {
     
     public void setCor(String cor) {
         this.cor = cor;
+    }
+    
+    public String getModelo() {
+    	return modelo;
+    }
+    
+    public void setModelo(String modelo) {
+    	this.modelo = modelo;
+    }
+    
+    public String getMarca() {
+    	return marca;
+    }
+    
+    public void setMarca(String marca) {
+    	this.marca = marca;
+    }
+    
+    public Long getAno() {
+    	return ano;
+    }
+    
+    public void setAno(Long ano) {
+    	this.ano = ano;
+    }
+    
+    public Long getQuantidade() {
+    	return quantidade;
+    }
+    
+    public void setQuantidade(Long quantidade) {
+    	this.quantidade = quantidade;
+    }
+    
+    public Date getDataEntrada() {
+    	return dataEntrada;
+    }
+    
+    public void setDataEntrada(Date dataEntrada) {
+    	this.dataEntrada = dataEntrada;
+    }
+    
+    public Long getPreco() {
+    	return preco;
+    }
+    
+    public void setPreco(Long preco) {
+    	this.preco = preco;
+    }
+    
+    public Long getFornecedorCnpj() {
+    	return fornecedorCnpj;
+    }
+    
+    public void setFornecedorCnpj(Long fornecedorCnpj) {
+    	this.fornecedorCnpj = fornecedorCnpj;
     }
 }
